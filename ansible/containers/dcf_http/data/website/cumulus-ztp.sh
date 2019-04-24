6ce9a501f1f486236730bc60f15bee618464e0ff
akarneliuk <a.karneliuk@gmail.com>
2019-04-24 18:09:14 +0200

    0.3.2

---
+++ ansible/containers/dcf_http/data/website/cumulus-ztp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# CUMULUS-AUTOPROVISIONING
+
+sleep 5
+
+net del dns nameserver ipv4 192.168.141.1
+net commit
+
+sleep 5
+
+net add vrf mgmt
+net add time zone 
+net add dns nameserver ipv4 192.168.141.1 vrf mgmt
+net commit
+
+adduser aaa --disabled-password --gecos "Network Admin User Account"
+adduser aaa sudo
+echo "aaa:aaa" | chpasswd
+
+exit 0
+
